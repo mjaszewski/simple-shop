766e868145d6f1a4fa4517ab8ee7b2dbdff571eb
mjaszewski <mateuszjaszewski@gmail.com>
2023-04-18 00:41:14 +0200

    fixes

--- src/helpers/arrayToObjectId.ts
+++ src/helpers/arrayToObjectId.ts
@@ -1,4 +1,4 @@
-export const arrayToObjectId = (array: any[], idKey: string = 'id') =>
+export const arrayToObjectId = (array: Record<string|number, any>[], idKey: string = 'id') =>
   array.reduce((target, item) => {
     target[item[idKey]] = item
 
